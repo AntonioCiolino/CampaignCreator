da5bba99f2b783aa4df8ae381473198a63fede27
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-03-18 12:15:27 -0400

    CRLF

--- CampaignCreator.py
+++ CampaignCreator.py
@@ -77,7 +77,7 @@ else:
 
     # AC: for now decided to totally regenerate the toc every time so we don't have to figure out if it's partial.
     if (st.button('Generate table of contents', help="Generates a table of contents.")):
-        st.session_state.toc = "Table of Contents:\n" +  Writing.Writing().generate_toc(st.session_state.campaign, model)
+        st.session_state.toc = "Table of Contents:\\n" +  Writing.Writing().generate_toc(st.session_state.campaign, model)
         st.write(st.session_state.toc)
     if (st.session_state.toc):
         st.session_state.toc = st.session_state.toc.replace('\\n', '  \\n')
