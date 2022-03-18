c6eca16625bfee00b2ef1263c353a36b3f21826b
Antonio Ciolino <antoniociolino@Antonios-MacBook-Pro.local>
2022-03-18 12:13:56 -0400

    CRLF

--- CampaignCreator.py
+++ CampaignCreator.py
@@ -5,6 +5,8 @@ import utils.Features as Features
 import utils.Tables as Tables
 import utils.Writing as Writing
 
+import re
+
 # check to see if this is making a difference.
 def update_content(args):
     pass
@@ -62,36 +64,10 @@ else:
     st.caption("choose which model that OpenAI will use to generate your content. Choose DaVinci, Curie, or your own fine tuned models.")
     model = st.selectbox("Select a model", st.session_state.models)
 
-    # with st.expander("Optional Tool: Inject random data"):
-    #     st.caption("Appends a random thing from the collection of options into the story area. This can be used to spark ideas for yourself or the generator.")
-    #     st.session_state.sel = st.selectbox('Select grouping of content', st.session_state.random_tables.keys(),
-    #                                         help="Select a random table to generate content from.")
-    #
-    #     # detemine button stuff before displaying or loading text boxes
-    #     if st.button('Inject a thing', help="Add a random thing to the content from a list of items."):
-    #         st.session_state.chapter += "\n" + Tables.Tables().get_random_thing()
-
-    # with st.expander("Optional tool: generate a style based on a specific sentence, phrase or idea."):
-    #     prompt = st.text_input('Prompt to process', '', help="If you have a specific short prompt, place it here to process. It will append the results to the story.")
-    #
-    #     # for the prompt, if the prompt is blank, disable the controls, but still render.
-    #     d = (prompt == "")
-    #     st.session_state.feat = st.selectbox('Select a style', st.session_state.features, disabled = d,
-    #                                          help="Requests data from GPT-3 in the selected style.")
-    #
-    #     col1, col2 = st.columns(2)
-    #     with col1:
-    #         if (st.button('Generate tuned content', help="Calls OpenAI for fine tuned content based on the prompt.", disabled = d)):
-    #             st.session_state.chapter += Writing.Writing().get_tuned_content(prompt, model)
-    #     with col2:
-    #         if (st.button('Generate generic content', help="(Shortcut) Calls OpenAI for Davinci content based no the prompt.", disabled = d)):
-    #             st.session_state.chapter += Writing.Writing().get_generic_content(prompt)
-
     st.info("""
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     
     """)
-
 
     concept = st.text_input('Idea for your campaign', '', key='concept')
     if (st.button('Generate campaign concept', help="This is the overall purpose of the campaign.")):
@@ -104,7 +80,7 @@ else:
         st.session_state.toc = "Table of Contents:\n" +  Writing.Writing().generate_toc(st.session_state.campaign, model)
         st.write(st.session_state.toc)
     if (st.session_state.toc):
-        st.session_state.toc = st.session_state.toc.replace('\n', ' HELLO ')
+        st.session_state.toc = st.session_state.toc.replace('\\n', ' HELLO ')
         st.write(st.session_state.toc)
         st.text_area('Table of Contents', '', key='toc')
 
